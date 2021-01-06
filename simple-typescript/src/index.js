98831a4fcd7a6cf1c97ac2e7c091745c245ea844
Ali Rajaii <rajaii_a@yahoo.com>
2021-01-06 11:28:09 -0500

    added PHP courses info and typscript course.  Will add all CE cdev courses here from now on to not flood my github with this stuff

---
+++ simple-typescript/src/index.js
@@ -0,0 +1,6 @@
+function multiply(a, b) {
+    return a * b;
+}
+var a = 2;
+var b = 2;
+console.log(multiply(a, b));
---
+++ simple-typescript/src/index.ts
@@ -0,0 +1,8 @@
+function multiply (a: number, b: number): number {
+ return a * b;
+}
+
+const a = 12;
+const b = 4;
+
+console.log(multiply(a,b));
