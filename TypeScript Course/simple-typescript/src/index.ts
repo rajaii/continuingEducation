476749d819fdf1af488b1189059bb4b46f655b51
Ali Rajaii <rajaii_a@yahoo.com>
2021-01-07 11:46:19 -0500

    interfaces notes in index.ts

--- TypeScript Course/simple-typescript/src/index.ts
+++ TypeScript Course/simple-typescript/src/index.ts
@@ -1,12 +1,68 @@
+// interface A {
+//     someProp: number;
+// }
+
+// interface B {
+//     someProp: number;
+//     anotherProp: number
+
+// }
+
+// let a: A = { someProp: 1 };
+// let b: B = a;
+
+// Interfaces
+interface Profile {
+    readonly name: string;
+    age?: number;
+
+}
+
+let profile: Profile = {
+    name: 'Ali',
+}
+
+profile.name = 'John';
+
+
+// Index Signature
+
 interface A {
-    someProp: number;
+    //index signature:
+    [key: string]: string | number;
+    someProp: string;
+    
 }
 
-interface B {
-    someProp: number;
-    anotherProp: number
+const a: A = {someProp: 'string'};
+a.x  = 1;
+a.y = 2;
 
+// Call Signature
+
+interface Sum {
+    (a: number, b: number): number;
+    prop1: string;
 }
 
-let a: A = { someProp: 1 };
-let b: B = a;
+const sum: Sum = (a,b) => a + b;
+sum.prop1 = 'number';
+
+
+// Extending Interfaces
+
+interface Parent {
+    a: string;
+}
+
+interface Parent2 {
+    y: string;
+}
+
+interface Parent3 {
+    z: string;
+}
+
+interface Child extends Parent, Parent2, Parent3 {}
+
+let child: Child = {a: 'something'}
