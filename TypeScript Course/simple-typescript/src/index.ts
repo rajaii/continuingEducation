5751ef21dc879af6ac26bf28bbec3d5cf05da087
Ali Rajaii <rajaii_a@yahoo.com>
2021-01-07 12:12:26 -0500

    functions notes added to index.tsc

--- TypeScript Course/simple-typescript/src/index.ts
+++ TypeScript Course/simple-typescript/src/index.ts
@@ -12,57 +12,84 @@
 // let b: B = a;
 
 // Interfaces
-interface Profile {
-    readonly name: string;
-    age?: number;
+// interface Profile {
+//     readonly name: string;
+//     age?: number;
 
-}
+// }
 
-let profile: Profile = {
-    name: 'Ali',
-}
+// let profile: Profile = {
+//     name: 'Ali',
+// }
 
-profile.name = 'John';
+// profile.name = 'John';
 
 
-// Index Signature
+// // Index Signature
 
-interface A {
-    //index signature:
-    [key: string]: string | number;
-    someProp: string;
+// interface A {
+//     //index signature:
+//     [key: string]: string | number;
+//     someProp: string;
     
+// }
+
+// const a: A = {someProp: 'string'};
+// a.x  = 1;
+// a.y = 2;
+
+// // Call Signature
+
+// interface Sum {
+//     (a: number, b: number): number;
+//     prop1: string;
+// }
+
+// const sum: Sum = (a,b) => a + b;
+// sum.prop1 = 'number';
+
+
+// // Extending Interfaces
+
+// interface Parent {
+//     a: string;
+// }
+
+// interface Parent2 {
+//     y: string;
+// }
+
+// interface Parent3 {
+//     z: string;
+// }
+
+// interface Child extends Parent, Parent2, Parent3 {}
+
+// let child: Child = {a: 'something'}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Functions:
+
+function sum(a: number, b?: number = 0) {
+    return a + b;
 }
 
-const a: A = {someProp: 'string'};
-a.x  = 1;
-a.y = 2;
+sum(1);
 
-// Call Signature
+type MyFunc = (a: number, b: number) => number;
 
-interface Sum {
-    (a: number, b: number): number;
-    prop1: string;
+
+const sum2: MyFunc = (a, b) => a + b;
+
+//unknown number of args
+
+function sumEverything(arg1: string, arg2: boolean, ...numbers: number[]): number {
+    return numbers.reduce((result, num) => result + num, 0)
 }
 
-const sum: Sum = (a,b) => a + b;
-sum.prop1 = 'number';
+// Overloads
 
+function add(a:string, b:string):string;
 
-// Extending Interfaces
-
-interface Parent {
-    a: string;
-}
-
-interface Parent2 {
-    y: string;
-}
-
-interface Parent3 {
-    z: string;
-}
-
-interface Child extends Parent, Parent2, Parent3 {}
-
-let child: Child = {a: 'something'}
+function add(a:number, b:number): number;
