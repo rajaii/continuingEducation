08670625abcc49f3419e3fa545b863a04707f262
Ali Rajaii <rajaii_a@yahoo.com>
2021-01-07 20:59:14 -0500

    notes for classes in tsc added

--- TypeScript Course/simple-typescript/src/index.ts
+++ TypeScript Course/simple-typescript/src/index.ts
@@ -71,25 +71,88 @@
 
 //Functions:
 
-function sum(a: number, b?: number = 0) {
-    return a + b;
+// function sum(a: number, b?: number = 0) {
+//     return a + b;
+// }
+
+// sum(1);
+
+// type MyFunc = (a: number, b: number) => number;
+
+
+// const sum2: MyFunc = (a, b) => a + b;
+
+// //unknown number of args
+
+// function sumEverything(arg1: string, arg2: boolean, ...numbers: number[]): number {
+//     return numbers.reduce((result, num) => result + num, 0)
+// }
+
+// // Overloads
+
+// function add(a:string, b:string):string;
+
+// function add(a:number, b:number): number;
+
+class Robot {
+    //protected makes it so you can access the prop in here or child classes can access within
+    //like private but extends to children of parent classes
+    // protected name: string;
+    _color: string;
+
+    static availableColors = ['green', 'yellow'];
+    static isColorAvailable(color: string) {
+        return Robot.availableColors.includes(color);
+    }
+
+    constructor(protected _name:string, _color: string) {
+        this._color = _color;
+    }
+
+    askName() {
+        console.log(`My name is ${this.name}`);
+    }
+
+    move(distance: number) {
+        console.log(`${this.name} moved ${distance} meters.`)
+    }
+
+    set color(color: string) {
+        if (!Robot.isColorAvailable(color)) {
+            throw new Error (`Color ${color} is not available.`);
+        }
+        this._color = color;
+    }
+
+    set name(value: string) {
+        this._name = 'PREFIX_' + value;
+    }
+
+    get name() {
+        return this._name + '_SUFFIX';
+    }
 }
 
-sum(1);
+class FlyingRobot extends Robot {
+    private readonly jetpackSize: number;
+    constructor(name: string, jetpackSize: number) {
+        super(name);
+        this.jetpackSize = jetpackSize;
+    }
 
-type MyFunc = (a: number, b: number) => number;
-
-
-const sum2: MyFunc = (a, b) => a + b;
-
-//unknown number of args
-
-function sumEverything(arg1: string, arg2: boolean, ...numbers: number[]): number {
-    return numbers.reduce((result, num) => result + num, 0)
+    move(distance: number) {
+        console.log(`${this.name} is flying.`);
+        super.move(distance);
+    }
 }
 
-// Overloads
+const robot = new Robot('john');
+robot.askName();
 
-function add(a:string, b:string):string;
+const flyingRobot = new FlyingRobot('Ali', 30);
+flyingRobot.move(10);
+// console.log(flyingRobot.jetpackSize) //because private line 114 err
+// console.log(robot.name) // because protected line 98 would err
 
-function add(a:number, b:number): number;
+flyingRobot.name = 'Ali';
+console.log(`My name is ${flyingRobot.name}`);
